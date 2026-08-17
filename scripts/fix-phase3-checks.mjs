7590f5a10448d6714f2b047148a26a3df40586f9
wallissonghost-code <wallissonghost@gmail.com>
2026-08-17 10:48:00 -0300

    chore: remove temporary phase3 check patch

--- scripts/fix-phase3-checks.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'node:fs';
-const p='scripts/check-architecture.mjs';
-let s=fs.readFileSync(p,'utf8');
-s=s.replaceAll("[['solo', solo], ['multiplayer server', mpServer]]","[['solo', soloContractView], ['multiplayer server', mpServer]]");
-s=s.replace("const soloContractView = solo+'\\n'+mobsSource+'\\n'+combatSource;","const soloContractView = solo+'\\n'+mobsSource+'\\n'+combatSource+'\\n'+read('src/core/contracts.mjs');");
-fs.writeFileSync(p,s);
